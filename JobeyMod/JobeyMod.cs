f29f9829f43c13ea3c685a31140cd7b7bc44291a
joseph <jdd1@vt.edu>
2023-12-05 19:45:21 -0500

    initial

---
+++ JobeyMod/JobeyMod.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using JobeyMod.Patches;
+
+namespace JobeyMod
+{
+    [BepInPlugin(modGuid, modName, modVersion)]
+    public class JobeyMod : BaseUnityPlugin
+    {
+        private const string modGuid = "JobeyMod.Plugin";
+        private const string modName = "Jobey Mod";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new(modGuid);
+
+        private static JobeyMod Instance;
+
+        internal ManualLogSource logSource;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            logSource = BepInEx.Logging.Logger.CreateLogSource(modGuid);
+
+            logSource.LogInfo("mod awake");
+
+            harmony.PatchAll(typeof(JobeyMod));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
---
+++ JobeyMod/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,21 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobeyMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void InfiniteSprintPatch(ref float ___sprintMeter)
+        {
+            ___sprintMeter = 1f;
+        }
+    }
+}
